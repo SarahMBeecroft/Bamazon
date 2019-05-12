6f1c5d1ebab063c0b9a56614bfe3206da0b3b460
Sarah B <smblair87@gmail.com>
2019-05-11 20:10:22 -0700

    Created function to show products using console.table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+var cTable = require('console.table');
 
 // Creates connection with MySQL
 var connection = mysql.createConnection({
@@ -8,16 +9,47 @@ var connection = mysql.createConnection({
   // Your port; if not 3306
   port: 3306,
 
-  // Your username
+  // Username
   user: 'root',
 
-  // Your password
+  // Password
   password: 'yourRootPassword',
-  database: "bamazon"
+
+  // Database
+  database: 'bamazon'
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
   connection.end();
 });
+
+
+// Function to show products
+function showProducts() {
+  
+  // Query for MySQL
+  var query = 'Select * FROM products';
+
+  connection.query(query, function (err, res) {
+
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+
+      // Displays data in a table
+      console.table([
+        {
+          ID: res[i].id,
+          Product: res[i].product_name,
+          Department: res[i].department_name,
+          Price: res[i].price,
+          Stock: res[i].stock_quantity,
+        }, 
+      ]);
+    }
+  });
+}
+
+showProducts();
