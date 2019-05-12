9fc550a62ca08af80fc1eb412acb49908cf32f6b
Sarah B <smblair87@gmail.com>
2019-05-12 16:39:24 -0700

    Added manager function to view all products and go back to main menu or exit

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,5 +1,6 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+var cTable = require('console.table');
 
 // Creates connection with MySQL
 var connection = mysql.createConnection({
@@ -30,7 +31,7 @@ function promptManager() {
       name: 'promptManager',
           type: 'list',
           message: '\n\nWhich action would you like to take?\n',
-          choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+          choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Exit']
     }
 ])
 .then(function(answer) {
@@ -38,8 +39,75 @@ function promptManager() {
   // Creates variables for user's input
   var managerAnswer = answer.promptManager;
 
+  // If statements to choose which function to run
+  if (managerAnswer === 'View Products for Sale'){
+    viewProducts();
+  }
+  else if (managerAnswer === 'View Low Inventory'){
+    viewLowInventory();
+  }
+  else if (managerAnswer === 'Add to Inventory') {
+    addInventory();
+  } 
+  else if (managerAnswer === 'Add New Product') {
+    addNewProduct();
+  }
+  else {
+    // Ends connection and shows good-bye message
+    connection.end();
+    console.log('\n\nGood-bye!');  
+  }
+
 })
 }
 
 
+// Function to view all products
+function viewProducts() {
+  
+  // Query for MySQL
+  var query = 'Select * FROM products';
 
+  connection.query(query, function (err, res) {
+
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+
+      // Displays data in a table
+      console.table([
+        {
+          ID: res[i].id,
+          Product: res[i].product_name,
+          Department: res[i].department_name,
+          Price: res[i].price,
+          Stock: res[i].stock_quantity,
+        }, 
+      ]);
+    }
+  });
+  // Prompts user to go back to main menu or exit app
+  inquirer.prompt([
+    {
+      name: 'stayOrExit',
+          type: 'list',
+          message: '\n\nWhich action would you like to take?\n',
+          choices: ['Go Back to Main Menu', 'Exit']
+    }
+]).then(function(answer) {
+
+  // Creates variables for user's input
+  var managerAnswer = answer.stayOrExit;
+
+  // If statements to choose which function to run
+  if (managerAnswer === 'Go Back to Main Menu'){
+    // Calls function to prompt manager
+    promptManager();
+  }
+  else {
+    // Ends connection and shows good-bye message
+    connection.end();
+    console.log('\n\nGood-bye!');  
+  }
+})
+}
